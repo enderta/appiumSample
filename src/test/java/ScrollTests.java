bce09a0dcf9dbd81e0f008b15715ee1b1b4c3fc2
enderta <endertanver@gmail.com>
2024-10-30 21:56:05 +0000

    ScrollTests

--- src/test/java/ScrollTests.java
+++ src/test/java/ScrollTests.java
@@ -3,7 +3,15 @@ import appium.driver.Driver;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.jupiter.api.Test;
-
+import org.openqa.selenium.Point;
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+import java.time.Duration;
+import java.util.Collections;
+import org.openqa.selenium.Point;
 import java.net.MalformedURLException;
 
 public class ScrollTests {
@@ -21,6 +29,11 @@ public class ScrollTests {
         Utils.tapElement(driver, driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"LOGIN\")")));
         Thread.sleep(2000);
         Utils.scrollToElement(driver,"$15.99");
+        Point start = new Point(0, 585);
+        Point end = new Point (571, 574);
+        Utils.swipe(driver, start, end);
+        WebElement logout = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"LOGOUT\"]"));
+        Utils.tapElement(driver, logout);
 
 
     }
